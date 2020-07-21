ace659abe5ef466d6d954788292acd5092a3d305
sdpb <sdpalaciob@eafit.edu.co>
2020-07-20 21:03:13 -0500

    News item parser and object creator

---
+++ NewsItem.py
@@ -0,0 +1,18 @@
+from textblob import TextBlob
+
+
+class NewsItem:
+
+    def __init__(self, parsed_article):
+        parsed_article.download()
+        parsed_article.parse()
+        parsed_article.nlp()
+        self.article_title = parsed_article.title
+        self.article_text = parsed_article.text
+        self.article_summary = parsed_article.summary
+        self.article_picture = parsed_article.top_image
+        obj = TextBlob(parsed_article.summary).translate(from_lang='auto', to='en')
+        self.article_score = obj.sentiment.polarity
+        self.article_subjectivity = obj.sentiment.subjectivity
+
+
