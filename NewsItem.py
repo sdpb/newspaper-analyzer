784f77ac17b1434b5c8997c6a534f4bf73c4a8cc
sdpb <sdpalaciob@eafit.edu.co>
2020-07-21 00:18:17 -0500

    Language verification, translate api issue

--- NewsItem.py
+++ NewsItem.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 
 class NewsItem:
 
-    def __init__(self, parsed_article):
+    def __init__(self, parsed_article, lang):
         parsed_article.download()
         parsed_article.parse()
         parsed_article.nlp()
@@ -11,7 +11,9 @@ class NewsItem:
         self.article_text = parsed_article.text
         self.article_summary = parsed_article.summary
         self.article_picture = parsed_article.top_image
-        obj = TextBlob(parsed_article.summary).translate(from_lang='auto', to='en')
+        obj = TextBlob(parsed_article.summary)
+        if lang != 'en':
+            obj = obj.translate(from_lang=lang, to='en')
         self.article_score = obj.sentiment.polarity
         self.article_subjectivity = obj.sentiment.subjectivity
 
