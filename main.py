f404ab2d13f96d2a0ec4c3f95a4e809f755c5679
mherreral <mherreral@eafit.edu.co>
2020-10-31 15:12:34 -0500

    setting the main file

---
+++ main.py
@@ -0,0 +1,11 @@
+from presentation.vars import arguments
+from presentation.visualization import results
+from presentation.user_interface import interface
+
+if __name__ == '__main__':
+    '''newspaper_arg, n_news = arguments.arguments()
+    url = newspaper_arg
+    # url = "https://www.eltiempo.com/"
+    # n_news = 10
+    results.exe(url, n_news)'''
+    interface
