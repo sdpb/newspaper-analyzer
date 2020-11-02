114d1f88d884321e9c8ac62cf67a23fabc845302
mherreral <mherreral@eafit.edu.co>
2020-11-02 14:30:12 -0500

    solved list issue

--- presentation/visualization/results.py
+++ presentation/visualization/results.py
@@ -11,6 +11,8 @@ def exe(newspaper_url, amount):
     filtered_articles = filter_articles(
         newspaper.articles[:amount], newspaper_url)
     parse_articles(filtered_articles)
+    # nuevo
+    score_news()
 
 
 def score_news():
@@ -20,15 +22,15 @@ def score_news():
         aux.append(_.article_title)
         aux.append(f'{round(score, 3)} {process_score(score)}')
         aux.append(f'{round(_.article_subjectivity, 3)}')
-        scored_news.append(aux)
+        copia = aux.copy()
+        scored_news.append(copia)
         aux.clear()
-
 
 
 def process_score(score):
     if -0.4 <= score <= 0.4:
-        return 'Neutral news.'
+        return 'neutral'
     elif score > 0.4:
-        return 'Positive news.'
+        return 'positive'
     elif score < -0.4:
-        return 'Negative news.'
+        return 'negative'
